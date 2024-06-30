66bd32fea1593ddc3ae24096c96efe92a8ae9cae
addy204 <aksharma@academic.rrc.ca>
2024-06-30 13:29:57 -0500

    Implemented multi-model data on member pages

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,48 +1,9 @@
-# app/controllers/breeds_controller.rb
 class BreedsController < ApplicationController
   def index
     @breeds = Breed.page(params[:page]).per(10)
   end
 
   def show
-    @breed = Breed.find(params[:id])
-  end
-
-  def new
-    @breed = Breed.new
-  end
-
-  def create
-    @breed = Breed.new(breed_params)
-    if @breed.save
-      redirect_to @breed
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @breed = Breed.find(params[:id])
-  end
-
-  def update
-    @breed = Breed.find(params[:id])
-    if @breed.update(breed_params)
-      redirect_to @breed
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @breed = Breed.find(params[:id])
-    @breed.destroy
-    redirect_to breeds_path
-  end
-
-  private
-
-  def breed_params
-    params.require(:breed).permit(:name, :sub_breeds)
+    @breed = Breed.includes(:owner, :dog_shows, :breed_facts).find(params[:id])
   end
 end
--- app/controllers/dog_shows_controller.rb
+++ app/controllers/dog_shows_controller.rb
@@ -1,48 +1,9 @@
-
 class DogShowsController < ApplicationController
   def index
     @dog_shows = DogShow.page(params[:page]).per(10)
   end
 
   def show
-    @dog_show = DogShow.find(params[:id])
-  end
-
-  def new
-    @dog_show = DogShow.new
-  end
-
-  def create
-    @dog_show = DogShow.new(dog_show_params)
-    if @dog_show.save
-      redirect_to @dog_show
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @dog_show = DogShow.find(params[:id])
-  end
-
-  def update
-    @dog_show = DogShow.find(params[:id])
-    if @dog_show.update(dog_show_params)
-      redirect_to @dog_show
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @dog_show = DogShow.find(params[:id])
-    @dog_show.destroy
-    redirect_to dog_shows_path
-  end
-
-  private
-
-  def dog_show_params
-    params.require(:dog_show).permit(:name, :date)
+    @dog_show = DogShow.includes(:breeds).find(params[:id])
   end
 end
